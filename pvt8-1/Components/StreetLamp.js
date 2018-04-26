d70197bae2bab385a9a6b91b519e3a69dec780ab
Lovelin <lovelindberg2@gmail.com>
2018-04-26 10:05:07 +0200

    ta bort

--- pvt8-1/Components/StreetLamp.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default  [
-
-  { id: '1', lat:59.345864, lng:18.033562, status: 'On'},
-  { id: '2', lat:59.338835, lng:18.036469, status: 'On'},
-  { id: '3', lat:59.342027, lng:18.047339, status: 'On'}
-
-]
